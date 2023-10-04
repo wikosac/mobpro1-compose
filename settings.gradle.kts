a1f4a38ffc4e5b1d30bd1b78ca661749288c955d
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-10-04 18:56:52 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Galeri Hewan"
+include(":app")
