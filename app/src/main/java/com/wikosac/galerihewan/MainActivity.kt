9a1b9c6d94fe42eb47a721fecccbc09136aa5cea
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-10-05 00:55:21 +0700

    Membuat item dapat diklik

--- app/src/main/java/com/wikosac/galerihewan/MainActivity.kt
+++ app/src/main/java/com/wikosac/galerihewan/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.wikosac.galerihewan
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,13 +17,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
@@ -43,7 +57,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
     private fun getData(): List<Hewan> {
         return listOf(
             Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
@@ -58,6 +71,10 @@ class MainActivity : ComponentActivity() {
             Hewan("Sapi", "Bos taurus", R.drawable.sapi),
         )
     }
+}
+
+private fun showToast(context: Context, text: String){
+    Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
 }
 
 @Composable
@@ -77,24 +94,30 @@ fun GaleriHewanApp(hewanList: List<Hewan>) {
 
 @Composable
 fun ListItem(hewan: Hewan) {
-    Row {
-        Image(
-            painter = painterResource(hewan.imageResId),
-            contentDescription = stringResource(id = R.string.gambar_hewan)
-        )
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 16.dp, top = 16.dp, bottom = 16.dp)
-        ) {
-            Text(
-                text = hewan.nama,
-                style = MaterialTheme.typography.titleLarge
+    val context = LocalContext.current
+    Surface(
+        modifier = Modifier.clickable { showToast(context, "${hewan.nama} diklik!") },
+        color = Color.White,
+    ) {
+        Row {
+            Image(
+                painter = painterResource(hewan.imageResId),
+                contentDescription = stringResource(id = R.string.gambar_hewan)
             )
-            Text(
-                text = hewan.namaLatin,
-                style = MaterialTheme.typography.bodySmall.copy(fontStyle = FontStyle.Italic)
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp, bottom = 16.dp)
+            ) {
+                Text(
+                    text = hewan.nama,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Text(
+                    text = hewan.namaLatin,
+                    style = MaterialTheme.typography.bodySmall.copy(fontStyle = FontStyle.Italic)
+                )
+            }
         }
     }
 }
