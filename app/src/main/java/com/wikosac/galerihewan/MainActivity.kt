f06b7011ac346780b9b57ee7a6e13806d505861c
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-10-04 19:58:21 +0700

    Menampilkan Nama Hewan Sesuai Urutan

--- app/src/main/java/com/wikosac/galerihewan/MainActivity.kt
+++ app/src/main/java/com/wikosac/galerihewan/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.wikosac.galerihewan
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,7 +16,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,6 +25,9 @@ import androidx.compose.ui.unit.dp
 import com.wikosac.galerihewan.ui.theme.GaleriHewanTheme
 
 class MainActivity : ComponentActivity() {
+    private val hewan = listOf("Ayam", "Bebek", "Domba", "Kambing", "Sapi")
+    private var index by mutableStateOf(0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -37,42 +37,50 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Hello World!")
+                    GaleriHewanApp(
+                        text = hewan[index],
+                        onClick = { showNext() }
+                    )
                 }
             }
         }
     }
+
+    private fun showNext() {
+        index++
+    }
 }
 
 @Composable
-fun Greeting(text: String, modifier: Modifier = Modifier) {
-
+fun GaleriHewanApp(
+    modifier: Modifier = Modifier,
+    text: String = stringResource(id = R.string.hewan_default),
+    onClick: () -> Unit
+) {
     Column(
-        modifier.fillMaxSize().padding(16.dp),
+        modifier
+            .fillMaxSize()
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = text)
         Button(
-            onClick = {
-                showNext()
-            },
-            modifier.fillMaxWidth(0.5f).padding(top = 16.dp),
+            onClick = { onClick() },
+            modifier
+                .fillMaxWidth(0.5f)
+                .padding(top = 16.dp),
             contentPadding = PaddingValues(16.dp)
         ) {
-            Text(text = stringResource(R.string.lanjut))
+            Text(text = stringResource(id = R.string.lanjut))
         }
     }
-}
-
-private fun showNext() {
-    Log.d("MainActivity", "Tombol diklik!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GaleriHewanTheme {
-        Greeting("Hello World!")
+        GaleriHewanApp(text = "Hello Wikosac!", onClick = {})
     }
 }
