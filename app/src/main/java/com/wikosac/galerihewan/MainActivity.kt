7d1a4914c9a845c37e511372688bcaaf306a753f
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-10-04 19:26:36 +0700

    Menambahkan Tombol Next

--- app/src/main/java/com/wikosac/galerihewan/MainActivity.kt
+++ app/src/main/java/com/wikosac/galerihewan/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.wikosac.galerihewan
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wikosac.galerihewan.ui.theme.GaleriHewanTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Hello World!")
                 }
             }
         }
@@ -30,17 +45,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(text: String, modifier: Modifier = Modifier) {
+
+    Column(
+        modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = text)
+        Button(
+            onClick = {
+                showNext()
+            },
+            modifier.fillMaxWidth(0.5f).padding(top = 16.dp),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            Text(text = stringResource(R.string.lanjut))
+        }
+    }
+}
+
+private fun showNext() {
+    Log.d("MainActivity", "Tombol diklik!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GaleriHewanTheme {
-        Greeting("Android")
+        Greeting("Hello World!")
     }
 }
