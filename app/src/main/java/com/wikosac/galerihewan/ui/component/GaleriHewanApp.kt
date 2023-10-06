7caf08291e64a92ec63902db5aa9cfb84c402d88
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-10-06 17:51:50 +0700

    Challenge

--- app/src/main/java/com/wikosac/galerihewan/ui/component/GaleriHewanApp.kt
+++ app/src/main/java/com/wikosac/galerihewan/ui/component/GaleriHewanApp.kt
@@ -1,17 +1,25 @@
 package com.wikosac.galerihewan.ui.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -28,14 +36,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.wikosac.galerihewan.R
 import com.wikosac.galerihewan.data.SettingDataStore
 import com.wikosac.galerihewan.data.dataStore
@@ -98,7 +112,7 @@ fun GaleriHewanApp(hewanList: List<Hewan>) {
                 columns = GridCells.Fixed(2)
             ) {
                 items(hewanList) { hewan ->
-                    ListItem(hewan)
+                    GridItem(hewan)
                 }
             }
         }
@@ -110,7 +124,7 @@ fun ListItem(hewan: Hewan) {
     val context = LocalContext.current
     Surface(
         modifier = Modifier.clickable { showToast(context, "${hewan.nama} diklik!") },
-        color = Color.White,
+        color = Color.White
     ) {
         Row {
             Image(
@@ -139,12 +153,62 @@ fun ListItem(hewan: Hewan) {
     }
 }
 
+@Composable
+fun GridItem(hewan: Hewan) {
+    val context = LocalContext.current
+    Surface(
+        modifier = Modifier
+            .clickable { showToast(context, "${hewan.nama} diklik!") }
+            .padding(4.dp),
+        color = Color.White
+    ) {
+        Box(
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Image(
+                painter = painterResource(id = hewan.imageResId),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.height(200.dp).width(200.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .background(Color(red = 0f, green = 0f, blue = 0f, alpha = 0.5f))
+                    .padding(8.dp)
+                    .width(200.dp),
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Text(
+                    text = hewan.nama,
+                    style = MaterialTheme.typography.titleLarge,
+                    color = Color.White
+                )
+                Text(
+                    text = hewan.namaLatin,
+                    style = MaterialTheme.typography.bodySmall.copy(fontStyle = FontStyle.Italic),
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun ListPreview() {
     GaleriHewanTheme {
         ListItem(
             Hewan("Domba", "Ovis ammon", R.drawable.domba)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GridPreview() {
+    GaleriHewanTheme {
+        GridItem(
+            Hewan("Bebek", "Cairina moschata", R.drawable.bebek)
         )
     }
 }
