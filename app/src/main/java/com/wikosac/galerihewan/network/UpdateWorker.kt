d2574cc7d3403704c8295a64b9fde1e133a892d8
wikosac <wesibrani@gmail.com>
2023-10-27 02:31:43 +0700

    Membuat notifikasi updater

--- app/src/main/java/com/wikosac/galerihewan/network/UpdateWorker.kt
+++ app/src/main/java/com/wikosac/galerihewan/network/UpdateWorker.kt
@@ -1,17 +1,44 @@
 package com.wikosac.galerihewan.network
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.content.Context
-import android.util.Log
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.wikosac.galerihewan.MainActivity
+import com.wikosac.galerihewan.R
 
 class UpdateWorker(
     context: Context,
     workerParams: WorkerParameters
 ) : Worker(context, workerParams) {
 
+    private val notificationId = 44
+
+    @SuppressLint("MissingPermission")
     override fun doWork(): Result {
-        Log.d("Worker", "Menjalankan proses background..")
+
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+        val pendingIntent = PendingIntent.getActivity(applicationContext,
+            0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(applicationContext,
+            MainActivity.CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(applicationContext.getString(R.string.notif_title))
+            .setContentText(applicationContext.getString(R.string.notif_text))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        val manager = NotificationManagerCompat.from(applicationContext)
+        manager.notify(notificationId, builder.build())
+
         return Result.success()
     }
 }
